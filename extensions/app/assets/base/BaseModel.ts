5aa87a6931459f4c17f9d0790851fe3e075350ca
张鹏 <didizhang@didiglobal.com>
2022-11-03 15:04:19 +0800

    update(更新): 提示

--- extensions/app/assets/base/BaseModel.ts
+++ extensions/app/assets/base/BaseModel.ts
@@ -1,3 +1,3 @@
 export type IModel<T> = {
-    [P in keyof T]: T[P] extends Function ? 'Model中不能定义方法' : T[P];
+    [P in keyof T]: T[P] extends Function ? 'Model中不能定义方法, 可以创建ModelManager负责管理Model' : T[P];
 };
