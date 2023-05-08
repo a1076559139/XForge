22fecd9304a1bf3e1872269a80b034a8b948acd1
张鹏 <didizhang@didiglobal.com>
2023-05-08 16:12:01 +0800

    version(版本): 2.2.19 update(更新): 更新lib.debug

--- extensions/app/assets/lib/debug/debug.ts
+++ extensions/app/assets/lib/debug/debug.ts
@@ -6,6 +6,12 @@ function jsGetSet(obj: unknown, prop: string, getter: Function, setter?: Functio
         configurable: false
     });
 }
+function clear(object: Record<string | number, any>) {
+    if (!object) return;
+    for (const key of Object.keys(object)) {
+        delete object[key];
+    }
+}
 /**
  * 将某个变量设置为不可观测(不可在浏览器中打印)
  * @param owner object | string | number | boolean | Array | Function | ...
@@ -16,7 +22,11 @@ export function unobservable(owner: unknown, callback?: Function) {
     if (!owner) return;
     function define() {
         function accessor() {
-            callback && callback();
+            if (callback) {
+                callback();
+            } else {
+                clear(owner);
+            }
         }
         try {
             jsGetSet(owner, 'unobservable', accessor.bind(null, 'de' + 'bu' + 'gg' + 'er'));
