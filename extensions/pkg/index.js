ddb9989f4e8b14d978f074796afc69377dafa000
zp <1076559139@qq.com>
2023-08-29 23:56:41 +0800

    version(版本): 3.1.1 update(更新): 优化package文件夹更新逻辑，避免编辑器报错

--- extensions/pkg/index.js
+++ extensions/pkg/index.js
@@ -25,38 +25,75 @@ async function spawnCmd(cmd, args, options) {
 }
 
 /**
- * 获得文件夹所有文件路径
- * @param {string} dir 
- * @returns {Array<string>}
+ * 同步文件夹内容
+ * @param {*} source 源
+ * @param {*} destination 目标
  */
-function getFiles(dir) {
-    const result = [];
-    if (fs.existsSync(dir) !== true) return result;
+function syncFolders(source, destination) {
+    // 删除在源中不存在的内容
+    function remove(source, destination) {
+        const files = fs.readdirSync(destination);
+        // 遍历目标文件夹中的每个文件或文件夹
+        files.forEach((file) => {
+            if (path.extname(file) === '.meta') {
+                const destinationPath = path.join(destination, file.slice(0, -5));
+                const destinationMetaPath = path.join(destination, file);
+                if (!fs.existsSync(destinationPath)) {
+                    fs.unlinkSync(destinationMetaPath);
+                }
+                return;
+            }
+            const sourcePath = path.join(source, file);
+            const destinationPath = path.join(destination, file);
+            const destinationMetaPath = destinationPath + '.meta';
 
-    const files = fs.readdirSync(dir);
-    files.forEach(function (item) {
-        var item_path = path.join(dir, item);
-        if (fs.statSync(item_path).isDirectory()) return;
-        result.push(item_path);
-    });
+            const stats = fs.statSync(destinationPath);
 
-    return result;
-}
+            // 检查目标文件夹中的项在源文件夹中是否存在
+            if (!fs.existsSync(sourcePath)) {
+                // 如果目标文件夹中的项在源文件夹中不存在，则删除(并删除对应的.meta)
+                if (fs.existsSync(destinationMetaPath)) {
+                    fs.unlinkSync(destinationMetaPath);
+                }
 
-/**
- * 遍历文件
- * @param {string} dir 
- * @param {( item_path: string, isDirectory: boolean ) => void} callback 
- * @returns 
- */
-function eachFiles(dir, callback) {
-    if (fs.existsSync(dir) !== true) return;
+                if (stats.isDirectory()) {
+                    deleteDirectory(destinationPath);
+                } else {
+                    fs.unlinkSync(destinationPath);
+                }
+            } else if (stats.isDirectory()) {
+                remove(sourcePath, destinationPath);
+            }
+        });
+    }
+    remove(source, destination);
 
-    const files = fs.readdirSync(dir);
-    files.forEach(function (item) {
-        const item_path = path.join(dir, item);
-        callback(item_path, fs.statSync(item_path).isDirectory());
-    });
+    function add(source, destination) {
+        // 获取源文件夹中的所有文件或文件夹
+        const sourceFiles = fs.readdirSync(source);
+
+        // 遍历源文件夹中的每个文件或文件夹
+        sourceFiles.forEach((file) => {
+            if (file === '.git') return;
+            if (file === '.package-lock.json') return;
+
+            const sourcePath = path.join(source, file);
+            const destinationPath = path.join(destination, file);
+
+            // 检查当前项是否是文件夹
+            if (fs.statSync(sourcePath).isDirectory()) {
+                // 如果是文件夹，则递归调用syncFolders
+                if (!fs.existsSync(destinationPath)) {
+                    fs.mkdirSync(destinationPath);
+                }
+                add(sourcePath, destinationPath);
+            } else {
+                // 如果是文件，则复制到目标文件夹
+                fs.copyFileSync(sourcePath, destinationPath);
+            }
+        });
+    }
+    add(source, destination);
 }
 
 /**
@@ -79,111 +116,64 @@ function deleteDirectory(dir) {
     fs.rmdirSync(dir);
 }
 
-/**
- * 拷贝源目录下的文件到目标目录下(过滤掉所有.git文件或目录)
- * @param {string} src  源
- * @param {string} dest 目标
- */
-function copyDirectory(src, dest) {
-    if (fs.existsSync(src) == false) {
-        return false;
-    }
-
-    if (fs.existsSync(dest) == false) {
-        fs.mkdirSync(dest);
-    }
-
-    // 拷贝新的内容进去
-    let dirs = fs.readdirSync(src);
-    dirs.forEach(function (item) {
-        if (item === '.git') return;
-        if (item === '.package-lock.json') return;
-        let item_path = path.join(src, item);
-        let temp = fs.statSync(item_path);
-        if (temp.isFile() || temp.isSymbolicLink()) { // 是文件
-            fs.copyFileSync(item_path, path.join(dest, item));
-        } else if (temp.isDirectory()) { // 是目录
-            copyDirectory(item_path, path.join(dest, item));
-        }
-    });
-}
-
 const assetsDir = path.join(__dirname, 'assets');
 const packageDir = path.join(__dirname, 'package');
 const modulesDir = path.join(packageDir, 'node_modules');
+const executorPath = path.join(__dirname, '../../assets/app-builtin/app-admin/executor.ts');
 
 async function main() {
-    // 存储meta信息
-    const metaMap = {};
-    eachFiles(assetsDir, (item_path, isDirectory) => {
-        if (isDirectory) {
-            if (!path.basename(item_path).startsWith('@')) return;
-            getFiles(item_path).forEach((item_path2) => {
-                if (!item_path2.endsWith('.meta')) return;
-                metaMap[item_path2] = fs.readFileSync(item_path2, 'utf-8');
-            });
-        } else if (item_path.endsWith('.meta')) {
-            metaMap[item_path] = fs.readFileSync(item_path, 'utf-8');
-        }
-    });
-
     // npm指令
     const npm = process.platform === 'win32' ? 'npm.cmd' : 'npm';
-    if (process.argv[2] === 'update') {
+    const cmd = process.argv[2].trim();
+
+    if (cmd === 'update') {
         const cmd = ['--registry=https://registry.npmjs.org', 'update', '--prefix', packageDir];
         const code = await spawnCmd(npm, cmd);
         if (code !== 0) console.error(`[失败]: ${code}`);
-    } else if (process.argv[2] === 'add') {
-        const cmd = ['--registry=https://registry.npmjs.org', 'install', '--prefix', packageDir];
-        if (process.argv[3]) cmd.push(process.argv[3]);
-        const code = await spawnCmd(npm, cmd);
+    } else if (cmd === 'add') {
+        const pkgName = process.argv[3].trim();
+        const args = ['--registry=https://registry.npmjs.org', 'install', '--prefix', packageDir];
+        if (pkgName) args.push(pkgName);
+        const code = await spawnCmd(npm, args);
         if (code !== 0) {
             console.error(`[失败]: ${code}`);
         } else {
-            // 直接更新executor.ts
-            const executorPath = path.join(__dirname, '../../assets/app-builtin/app-admin/executor.ts');
+            // 直接更新executor.ts避免安装后直接在vscode中无法触发智能提示
             if (fs.existsSync(executorPath)) {
                 const str = fs.readFileSync(executorPath, 'utf-8');
-                if (str.search(new RegExp(`import\\s+['"]db://pkg/${process.argv[3]}['"]`)) === -1) {
-                    fs.writeFileSync(executorPath, str + `\nimport 'db://pkg/${process.argv[3]}'`, 'utf-8');
+                const reg = new RegExp(`import\\s+['"]db://pkg/${pkgName}['"]`)
+                if (str.search(reg) === -1) {
+                    fs.writeFileSync(executorPath, str + `\nimport 'db://pkg/${pkgName}'`, 'utf-8');
                 }
             }
         }
-    } else if (process.argv[2] === 'remove') {
-        const cmd = ['--registry=https://registry.npmjs.org', 'uninstall', '--prefix', packageDir];
-        if (!process.argv[3]) return console.error('[失败]: 输入要卸载的名字');
-        cmd.push(process.argv[3]);
-        const code = await spawnCmd(npm, cmd);
+    } else if (cmd === 'remove') {
+        const pkgName = process.argv[3].trim();
+        const pkgDir = path.join(modulesDir, pkgName);
+        const args = ['--registry=https://registry.npmjs.org', 'uninstall', '--prefix', packageDir];
+        if (!pkgName) return console.error('[失败]: 输入要卸载的名字');
+        args.push(pkgName);
+        const code = await spawnCmd(npm, args);
         if (code !== 0) {
             console.error(`[失败]: ${code}`);
         } else {
-            if (fs.existsSync(path.join(modulesDir, process.argv[3].trim()))) {
-                // 如果文件夹未删除成功 则 强制删除
-                deleteDirectory(path.join(modulesDir, process.argv[3].trim()));
+            // 如果文件夹未删除成功 则 强制删除
+            if (fs.existsSync(pkgDir)) {
+                deleteDirectory(pkgDir);
             }
             // 直接更新executor.ts避免返回编辑器报错
-            const executorPath = path.join(__dirname, '../../assets/app-builtin/app-admin/executor.ts');
             if (fs.existsSync(executorPath)) {
                 const str = fs.readFileSync(executorPath, 'utf-8');
-                fs.writeFileSync(executorPath, str.replace(new RegExp(`import\\s+['"]db://pkg/${process.argv[3]}['"]`), ''), 'utf-8');
+                const reg = new RegExp(`import\\s+['"]db://pkg/${pkgName}['"]`);
+                fs.writeFileSync(executorPath, str.replace(reg, ''), 'utf-8');
             }
         }
     } else {
         return console.error('[未知指令]');
     }
 
-    deleteDirectory(assetsDir);
-    copyDirectory(modulesDir, assetsDir);
-
-    // 还原meta，或删除无用meta
-    for (const metaPath in metaMap) {
-        if (!fs.existsSync(metaPath.slice(0, -5))) {
-            if (!fs.existsSync(metaPath)) continue;
-            fs.unlinkSync(metaPath);
-        } else {
-            fs.writeFileSync(metaPath, metaMap[metaPath], 'utf-8');
-        }
-    }
+    // 同步文件
+    syncFolders(modulesDir, assetsDir);
 
     console.log('⚠️: 如果编辑器报错，请点击资源管理器右上角的刷新按钮');
     console.log('⚠️: 如果运行时代码没更新，请点击编辑器菜单「开发者->缓存->清除代码缓存」');
