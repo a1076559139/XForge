c0f026f3d56a6dc68fe28d727b8d1b1d11d2d232
张鹏 <didizhang@didiglobal.com>
2024-01-26 18:09:07 +0800

    no message

--- extensions/app/assets/base/BaseModel.ts
+++ extensions/app/assets/base/BaseModel.ts
@@ -1,27 +1,39 @@
+// export type IModel<T> = {
+//     [P in keyof T]: T[P] extends Function
+//     ? '❌此处不能定义任何方法'
+//     : (
+//         T[P] extends Array<infer R>
+//         ? (
+//             R extends Function
+//             ? '❌此处不能定义任何方法'
+//             : T[P]
+//         )
+//         : T[P] // IModel<T[P]> 性能消耗大
+//     );
+// };
+
+// export type IStore<T> = {
+//     [P in keyof T]: T[P] extends Function
+//     ? T[P]
+//     : (
+//         T[P] extends Array<infer R>
+//         ? (
+//             R extends Function
+//             ? '❌此处不能定义任何方法'
+//             : IModel<T[P]>
+//         )
+//         : IModel<T[P]>
+//     );
+// };
+
 export type IModel<T> = {
     [P in keyof T]: T[P] extends Function
     ? '❌此处不能定义任何方法'
-    : (
-        T[P] extends Array<infer R>
-        ? (
-            R extends Function
-            ? '❌此处不能定义任何方法'
-            : T[P]
-        )
-        : T[P] // IModel<T[P]> 性能消耗大
-    );
+    : T[P];
 };
 
 export type IStore<T> = {
     [P in keyof T]: T[P] extends Function
     ? T[P]
-    : (
-        T[P] extends Array<infer R>
-        ? (
-            R extends Function
-            ? '❌此处不能定义任何方法'
-            : IModel<T[P]>
-        )
-        : IModel<T[P]>
-    );
+    : IModel<T[P]>;
 };
