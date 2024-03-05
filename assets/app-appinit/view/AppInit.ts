a189e14f7446c64b146a3ea112b856de09b9cec9
zp <1076559139@qq.com>
2024-03-06 01:45:14 +0800

    update(更新): 首屏展示logo

--- assets/app-appinit/view/AppInit.ts
+++ assets/app-appinit/view/AppInit.ts
@@ -1,14 +1,37 @@
-import { _decorator } from 'cc';
+import { Node, UIOpacity, _decorator, tween } from 'cc';
 import BaseAppInit from '../../../extensions/app/assets/base/BaseAppInit';
-const { ccclass } = _decorator;
+const { ccclass, property } = _decorator;
 
 @ccclass('AppInit')
 export class AppInit extends BaseAppInit {
-    onLoad() {
-        // 执行初始化操作
+    @property(Node)
+    private logo: Node;
+
+    /**
+    * 获得用户资源总量，这里返回几，就需要用户自行调用几次nextInit
+    */
+    protected getUserAssetNum(): number {
+        return 1;
     }
 
-    onFinish() {
+    protected onLoad() {
+        // 执行初始化操作
+        const opacity = this.logo.getComponent(UIOpacity);
+        opacity.opacity = 0;
+        tween(opacity)
+            .to(0.5, { opacity: 255 })
+            .delay(1)
+            .to(0.5, { opacity: 0 })
+            .call(() => {
+                this.nextInit();
+            })
+            .start();
+    }
+
+    // BaseAppInit中使用start方法作为初始化入口，如果重写start方法，请注意调用父类方法
+    // protected start() {  }
+
+    protected onFinish() {
         // 执行完成操作
         this.node.destroy();
     }
