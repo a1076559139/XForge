87c02bf1e8ca6de92a01d907bbe0e5bf2367d9ef
zp <1076559139@qq.com>
2024-04-26 00:19:11 +0800

    update(更新): app.scene改为readonly

--- extensions/app/engine/src/main.ts
+++ extensions/app/engine/src/main.ts
@@ -194,6 +194,7 @@ async function clearExecutor() {
     result += '    manager: {},\n';
     result += '    data: {},\n';
     result += '    config: {}\n';
+    result += '    store: {}\n';
     result += '    scene: IViewName[]\n';
     result += '}\n';
 
@@ -449,7 +450,7 @@ async function updateExecutor() {
     result += `    data: {${dataList.map(varname => `${varname.slice(5)}:${varname}`).join(',')}},\n`;
     result += `    config: {${confList.map(varname => `${varname.slice(7)}:IReadOnly<${varname}>`).join(',')}}\n`;
     result += `    store: {${storeList.map(varname => `${varname.slice(6)}:IReadOnly<${varname}>`).join(',')}}\n`;
-    result += '    scene: IViewName[]\n';
+    result += '    scene: IReadOnly<IViewName[]>\n';
     result += '}\n';
 
     // 修正windows系统中的\为/
